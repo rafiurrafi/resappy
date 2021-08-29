e73134c8feff71d314ef7dc53dae5497c5d25cb9
Rafiur Rahman <rrfoundary@gmail.com>
2021-08-29 21:57:16 +0600

    added animation

--- js/complex-slider.js
+++ js/complex-slider.js
@@ -1,9 +1,21 @@
+const numbers = document.querySelectorAll(".complex-slider__number");
+const texts = document.querySelectorAll(".complex-slider__text");
+const numberContainer = document.querySelector(".complex-slider__number-box");
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
+
+  //faalling
+  const cloneNumber = numbers[n - 1].cloneNode(true);
+  cloneNumber.style.color = "red";
+  cloneNumber.className == "complex-slider__number-clone";
+  cloneNumber.style.animationName = "moveToBottom";
+  numberContainer.appendChild(cloneNumber);
+  console.log(cloneNumber);
 }
 
 // Thumbnail image controls
@@ -14,9 +26,6 @@ function showSlides(n) {
   var i;
   // var slides = document.getElementsByClassName("mySlides");
   // var dots = document.getElementsByClassName("dot");
-  const numbers = document.querySelectorAll(".complex-slider__number");
-  const texts = document.querySelectorAll(".complex-slider__text");
-  const numberContainer = document.querySelector(".complex-slider__numbers");
 
   if (n > 5) {
     return;
@@ -31,12 +40,9 @@ function showSlides(n) {
   // for (i = 0; i < dots.length; i++) {
   //     dots[i].className = dots[i].className.replace(" active", "");
   // }
+
   numbers[slideIndex - 1].style.opacity = 1;
   numbers[slideIndex - 1].style.visibility = "visible";
   texts[slideIndex - 1].style.opacity = 1;
   numbers[slideIndex - 1].style.visibility = "visible";
-
-  const cloneNumber = numbers[n - 1].cloneNode(true);
-  numberContainer.appendChild(cloneNumber);
-  console.log(cloneNumber);
 }
