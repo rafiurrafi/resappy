4d3ae7f1527659d3170905bafbec137d3e0b290d
Rafiur Rahman <rrfoundary@gmail.com>
2021-09-02 19:27:33 +0600

    complex slider working properly

--- js/complex-slider-backup.js
+++ js/complex-slider-backup.js
@@ -1,73 +1,193 @@
-// const numbers = document.querySelectorAll(".complex-slider__number");
-// const numbersContainer = document.querySelector(".complex-slider__number-area");
-// const imageToSlide = document.querySelector(".complex-slider__images");
-// const sliderControls = document.querySelectorAll(".complex-slider__control");
-// const sliderControlContainer = document.querySelectorAll(
-//   ".complex-slider__controls div"
-// );
-// //slider style
-// const complexSliderPrev = document.querySelector("#cs-prev");
-// const complexSliderNext = document.querySelector("#cs-next");
-// complexSliderPrev.style.opacity = 0;
-// let currentActiveControl = 0;
-// sliderControls[currentActiveControl].classList.add("active");
+// Declare variable
+const numbers = document.querySelectorAll(".complex-slider__number");
+const numbersContainer = document.querySelector(".complex-slider__number-area");
+const imageToSlide = document.querySelector(".complex-slider__images");
+const sliderContents = document.querySelectorAll(".complex-slider__content");
+const sliderControls = document.querySelectorAll(".complex-slider__control");
+const optionalImage = document.querySelector(".complex-slider__optional-img");
+const sliderControlContainer = document.querySelectorAll(
+  ".complex-slider__controls div"
+);
 
-// for (let i = 0; i < numbers.length; i++) {
-//   numbers[i].style.opacity = 0;
-// }
-// numbers[0].style.opacity = 1;
-// var count = 0;
-// console.log(numbersContainer);
-// //next slide function
-// function nextSlide(n) {
-//   if (count > 4) return;
-//   count += n;
-//   console.log(count);
-//   for (let i = 0; i < numbers.length; i++) {
-//     numbers[i].style.opacity = 0;
-//   }
-//   setTimeout(() => {
-//     numbers[count].style.opacity = 1;
-//   }, 100);
-//   // vertical slider
-//   imageToSlide.style.transform =
-//     "translate(" + 0 + "rem," + -count * 37 + "rem)";
+//slider style
+const complexSliderPrev = document.querySelector("#cs-prev");
+const complexSliderNext = document.querySelector("#cs-next");
 
-//   //clone and falling animation
-//   const cloneNumber = numbers[count - 1].cloneNode(true);
-//   // if (count !== 1) cloneNumber.style.animationDelay = ".1s";
-//   cloneNumber.style.opacity = 1;
-//   if (!(count % 2))
-//     cloneNumber.classList.add("complex-slider__number-clone--1");
-//   else cloneNumber.classList.add("complex-slider__number-clone--2");
-//   numbersContainer.appendChild(cloneNumber);
-//   setTimeout(() => {
-//     cloneNumber.remove();
-//   }, 400);
-//   //slider next prev icon
-//   if (count == 0) complexSliderPrev.style.opacity = 0;
-//   else complexSliderPrev.style.opacity = 1;
-//   console.log(complexSliderPrev, complexSliderNext);
-// }
-// //function what happend when click on slider control
-// function goSlider(slideNo, evt) {
-//   //create slider control clone
-//   const cloneControl = sliderControls[currentActiveControl].cloneNode(true);
-//   cloneControl.classList.add("complex-slider__control--clone");
-//   cloneControl.style.transitionDelay = ".2s";
-//   cloneControl.style.transform = "translateX(" + 100 + "px)";
-//   sliderControlContainer[currentActiveControl].appendChild(cloneControl);
+//initialize slider
+let currentActiveControl = 0;
 
-//   // const cloneControl =
-//   //end slider control clone
-//   //main slider function
-//   for (let i = 0; i < numbers.length; i++) {
-//     numbers[i].style.opacity = 0;
-//   }
-//   numbers[slideNo - 1].style.opacity = 1;
-//   // vertical slider
-//   imageToSlide.style.transform =
-//     "translate(" + 0 + "rem," + -(slideNo - 1) * 37 + "rem)";
-// }
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].style.opacity = 0;
+  sliderContents[i].style.opacity = 0;
+}
+numbers[0].style.opacity = 1;
+sliderContents[0].style.opacity = 1;
+var count = 0;
+complexSliderPrev.style.opacity = 0;
 
-//function on active slide control
+// add next slider event handler
+function nextSlide() {
+  //endpoint
+  if (count > 3) {
+    complexSliderNext.classList.add("cs-next-end");
+    complexSliderNext.setAttribute("onclick", "prevSlide()");
+    complexSliderPrev.style.opacity = 0;
+    console.log(count);
+    return;
+  }
+  //hide all numbers
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].style.opacity = 0;
+  }
+
+  // Show item
+  setTimeout(() => {
+    numbers[count].style.opacity = 1;
+  }, 200);
+
+  // vertical slider
+  imageToSlide.style.transform =
+    "translate(" + 0 + "rem," + -(count + 1) * 37 + "rem)";
+
+  //show contents
+  for (let i = 0; i < sliderContents.length; i++)
+    sliderContents[i].style.opacity = 0;
+
+  setTimeout(() => {
+    sliderContents[count].style.opacity = 1;
+  }, 150);
+
+  //clone and falling animation
+  const cloneNumber = numbers[count].cloneNode(true);
+  // if (count !== 1) cloneNumber.style.animationDelay = ".1s";
+  cloneNumber.style.opacity = 1;
+  if (!(count % 2))
+    cloneNumber.classList.add("complex-slider__number-clone--1");
+  else cloneNumber.classList.add("complex-slider__number-clone--2");
+  numbersContainer.appendChild(cloneNumber);
+  setTimeout(() => {
+    cloneNumber.remove();
+  }, 400);
+
+  //Clone the control
+  const controlClone = document.querySelector(
+    ".complex-slider__control--clone"
+  );
+  controlClone.style.transform = "translateX(" + 5.7 * (count + 1) + "rem)";
+
+  //show optional image
+  if (count === 0 || count === 3) optionalImage.style.opacity = 1;
+  else optionalImage.style.opacity = 0;
+
+  //   increment count
+  count += 1;
+  currentActiveControl = count;
+  complexSliderPrev.style.opacity = 1;
+}
+
+// add pev slider event handler
+function prevSlide() {
+  // Endpoint s
+  if (count <= 1) {
+    complexSliderPrev.style.opacity = 0;
+    return;
+  }
+  if (count == 4) {
+    complexSliderNext.setAttribute("onclick", "nextSlide()");
+    complexSliderNext.classList.remove("cs-next-end");
+    complexSliderPrev.style.opacity = 1;
+
+    console.log(complexSliderNext);
+  }
+
+  //hide all numberss
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].style.opacity = 0;
+  }
+
+  setTimeout(() => {
+    numbers[count].style.opacity = 1;
+  }, 150);
+  // vertical slider
+  imageToSlide.style.transform =
+    "translate(" + 0 + "rem," + -(count - 1) * 37 + "rem)";
+
+  //show contents
+  for (let i = 0; i < sliderContents.length; i++)
+    sliderContents[i].style.opacity = 0;
+
+  setTimeout(() => {
+    sliderContents[count].style.opacity = 1;
+  }, 150);
+
+  //clone and falling animation
+  const cloneNumber = numbers[count].cloneNode(true);
+  // if (count !== 1) cloneNumber.style.animationDelay = ".1s";
+  cloneNumber.style.opacity = 1;
+  if (!(count % 2))
+    cloneNumber.classList.add("complex-slider__number-clone--1");
+  else cloneNumber.classList.add("complex-slider__number-clone--2");
+  numbersContainer.appendChild(cloneNumber);
+  setTimeout(() => {
+    cloneNumber.remove();
+  }, 400);
+
+  //Clone the control
+  const controlClone = document.querySelector(
+    ".complex-slider__control--clone"
+  );
+  controlClone.style.transform = "translateX(" + 5.7 * (count - 1) + "rem)";
+
+  //show optional image
+  if (count === 0 || count === 3) optionalImage.style.opacity = 1;
+  else optionalImage.style.opacity = 0;
+
+  //   increment count
+  count -= 1;
+  currentActiveControl = count;
+}
+
+// direct access function
+//function what happend when click on slider control
+function goSlider(slideNo, evt) {
+  //add active class to slider control
+  // for (let i = 0; i < sliderContents.length; i++)
+  //   sliderControls[i].classList.remove("active");
+  // sliderControls[slideNo - 1].classList.add("active");
+  //Clone the control
+  const controlClone = document.querySelector(
+    ".complex-slider__control--clone"
+  );
+  controlClone.style.transform = "translateX(" + 5.7 * (slideNo - 1) + "rem)";
+  // Hide all numbers
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].style.opacity = 0;
+  }
+  // vertical slider
+  imageToSlide.style.transform =
+    "translate(" + 0 + "rem," + -(slideNo - 1) * 37 + "rem)";
+  //show contents
+  for (let i = 0; i < sliderContents.length; i++)
+    sliderContents[i].style.opacity = 0;
+  setTimeout(() => {
+    sliderContents[slideNo - 1].style.opacity = 1;
+    numbers[slideNo - 1].style.opacity = 1;
+  }, 150);
+  //clone and falling animation
+  const cloneNumber = numbers[currentActiveControl].cloneNode(true);
+  // if (count !== 1) cloneNumber.style.animationDelay = ".1s";
+  cloneNumber.style.opacity = 1;
+  if (!(count % 2))
+    cloneNumber.classList.add("complex-slider__number-clone--1");
+  else cloneNumber.classList.add("complex-slider__number-clone--2");
+  numbersContainer.appendChild(cloneNumber);
+  setTimeout(() => {
+    cloneNumber.remove();
+  }, 700);
+
+  //show optional image
+  if (count === 0 || count === 3) optionalImage.style.opacity = 1;
+  else optionalImage.style.opacity = 0;
+
+  currentActiveControl = slideNo - 1;
+}
--- js/complex-slider.js
+++ js/complex-slider.js
@@ -18,105 +18,47 @@ let currentActiveControl = 0;
 
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].style.opacity = 0;
+  sliderContents[i].style.opacity = 0;
 }
 numbers[0].style.opacity = 1;
+sliderContents[0].style.opacity = 1;
 var count = 0;
 complexSliderPrev.style.opacity = 0;
 
 // add next slider event handler
 function nextSlide() {
-  if (count >= 4) {
+  complexSliderPrev.style.opacity = 1;
+  count += 1;
+  if (count === 5) {
     complexSliderNext.classList.add("cs-next-end");
     complexSliderNext.setAttribute("onclick", "prevSlide()");
     complexSliderPrev.style.opacity = 0;
-    console.log(count);
+  }
+  if (count > 5) {
+    complexSliderNext.classList.add("cs-next-end");
+    complexSliderNext.setAttribute("onclick", "prevSlide()");
+    complexSliderPrev.style.opacity = 0;
     return;
   }
-  if (count > 4) {
-    return;
-  }
+  console.log(count);
+  //hide all numbers
   for (let i = 0; i < numbers.length; i++) {
     numbers[i].style.opacity = 0;
+    sliderContents[i].style.opacity = 0;
   }
 
   // Show item
   setTimeout(() => {
     numbers[count].style.opacity = 1;
+    sliderContents[count].style.opacity = 1;
   }, 200);
 
   // vertical slider
   imageToSlide.style.transform =
-    "translate(" + 0 + "rem," + -(count + 1) * 37 + "rem)";
-
-  //show contents
-  for (let i = 0; i < sliderContents.length; i++)
-    sliderContents[i].style.opacity = 0;
-  setTimeout(() => {
-    sliderContents[count].style.opacity = 1;
-  }, 150);
+    "translate(" + 0 + "rem," + -count * 37 + "rem)";
 
   //clone and falling animation
-  const cloneNumber = numbers[count].cloneNode(true);
-  // if (count !== 1) cloneNumber.style.animationDelay = ".1s";
-  cloneNumber.style.opacity = 1;
-  if (!(count % 2))
-    cloneNumber.classList.add("complex-slider__number-clone--1");
-  else cloneNumber.classList.add("complex-slider__number-clone--2");
-  numbersContainer.appendChild(cloneNumber);
-  setTimeout(() => {
-    cloneNumber.remove();
-  }, 700);
-
-  //Clone the control
-  const controlClone = document.querySelector(
-    ".complex-slider__control--clone"
-  );
-  controlClone.style.transform = "translateX(" + 5.7 * (count + 1) + "rem)";
-
-  //show optional image
-  if (count === 0 || count === 3) optionalImage.style.opacity = 1;
-  else optionalImage.style.opacity = 0;
-
-  //   increment count
-  count += 1;
-  currentActiveControl = count;
-  complexSliderPrev.style.opacity = 1;
-}
-
-// add pev slider event handler
-function prevSlide() {
-  if (count <= 0) {
-    complexSliderPrev.style.opacity = 0;
-    return;
-  }
-
-  if (count == 4) {
-    complexSliderNext.setAttribute("onclick", "nextSlide()");
-    complexSliderNext.classList.remove("cs-next-end");
-    complexSliderPrev.style.opacity = 1;
-
-    console.log(complexSliderNext);
-  }
-
-  for (let i = 0; i < numbers.length; i++) {
-    numbers[i].style.opacity = 0;
-  }
-  setTimeout(() => {
-    numbers[count].style.opacity = 1;
-  }, 150);
-  // vertical slider
-  imageToSlide.style.transform =
-    "translate(" + 0 + "rem," + -(count - 1) * 37 + "rem)";
-
-  //show contents
-  for (let i = 0; i < sliderContents.length; i++)
-    sliderContents[i].style.opacity = 0;
-  setTimeout(() => {
-    sliderContents[count].style.opacity = 1;
-  }, 150);
-
-  //clone and falling animation
-  const cloneNumber = numbers[count].cloneNode(true);
+  const cloneNumber = numbers[count - 1].cloneNode(true);
   // if (count !== 1) cloneNumber.style.animationDelay = ".1s";
   cloneNumber.style.opacity = 1;
   if (!(count % 2))
@@ -131,43 +73,88 @@ function prevSlide() {
   const controlClone = document.querySelector(
     ".complex-slider__control--clone"
   );
-  controlClone.style.transform = "translateX(" + 5.7 * (count - 1) + "rem)";
+  controlClone.style.transform = "translateX(" + 5.7 * count + "rem)";
 
   //show optional image
-  if (count === 0 || count === 3) optionalImage.style.opacity = 1;
+  if (count === 0 || count === 4) optionalImage.style.opacity = 1;
   else optionalImage.style.opacity = 0;
+  currentActiveControl = count;
+}
 
+// add pev slider event handler
+function prevSlide() {
+  if (count === 1) {
+    complexSliderPrev.style.opacity = 0;
+    // return;
+  } else if (count == 0) return;
   //   increment count
   count -= 1;
+  if (count === 4) {
+    complexSliderNext.classList.remove("cs-next-end");
+    complexSliderNext.setAttribute("onclick", "nextSlide()");
+    complexSliderPrev.style.opacity = 1;
+  }
+
+  //hide all numberss
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].style.opacity = 0;
+    sliderContents[i].style.opacity = 0;
+  }
+
+  setTimeout(() => {
+    numbers[count].style.opacity = 1;
+    sliderContents[count].style.opacity = 1;
+  }, 150);
+  // vertical slider
+  imageToSlide.style.transform =
+    "translate(" + 0 + "rem," + -count * 37 + "rem)";
+
+  //clone and falling animation
+  const cloneNumber = numbers[count].cloneNode(true);
+  cloneNumber.style.opacity = 1;
+  if (!(count % 2))
+    cloneNumber.classList.add("complex-slider__number-clone--1");
+  else cloneNumber.classList.add("complex-slider__number-clone--2");
+  numbersContainer.appendChild(cloneNumber);
+  setTimeout(() => {
+    cloneNumber.remove();
+  }, 400);
+
+  //Clone the control
+  const controlClone = document.querySelector(
+    ".complex-slider__control--clone"
+  );
+  controlClone.style.transform = "translateX(" + 5.7 * count + "rem)";
+
+  //show optional image
+  if (count === 0 || count === 4) optionalImage.style.opacity = 1;
+  else optionalImage.style.opacity = 0;
   currentActiveControl = count;
 }
 
 // direct access function
 //function what happend when click on slider control
-function goSlider(slideNo, evt) {
-  //add active class to slider control
-  // for (let i = 0; i < sliderContents.length; i++)
-  //   sliderControls[i].classList.remove("active");
-  // sliderControls[slideNo - 1].classList.add("active");
-  //Clone the control
+function goSlider(slideNo) {
   const controlClone = document.querySelector(
     ".complex-slider__control--clone"
   );
   controlClone.style.transform = "translateX(" + 5.7 * (slideNo - 1) + "rem)";
+
   // Hide all numbers
   for (let i = 0; i < numbers.length; i++) {
     numbers[i].style.opacity = 0;
-  }
-  // vertical slider
-  imageToSlide.style.transform =
-    "translate(" + 0 + "rem," + -(slideNo - 1) * 37 + "rem)";
-  //show contents
-  for (let i = 0; i < sliderContents.length; i++)
     sliderContents[i].style.opacity = 0;
+  }
+
   setTimeout(() => {
     sliderContents[slideNo - 1].style.opacity = 1;
     numbers[slideNo - 1].style.opacity = 1;
   }, 150);
+
+  // vertical slider
+  imageToSlide.style.transform =
+    "translate(" + 0 + "rem," + -(slideNo - 1) * 37 + "rem)";
+
   //clone and falling animation
   const cloneNumber = numbers[currentActiveControl].cloneNode(true);
   // if (count !== 1) cloneNumber.style.animationDelay = ".1s";
@@ -181,7 +168,7 @@ function goSlider(slideNo, evt) {
   }, 700);
 
   //show optional image
-  if (count === 0 || count === 3) optionalImage.style.opacity = 1;
+  if (count === 0 || count === 4) optionalImage.style.opacity = 1;
   else optionalImage.style.opacity = 0;
 
   currentActiveControl = slideNo - 1;
